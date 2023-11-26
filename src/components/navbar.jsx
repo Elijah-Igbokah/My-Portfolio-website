d8bddd9290c89b203e2208f6eef407bb009dd72b
Elijah Igbokah <igbokah@gmail.com>
2023-11-26 22:21:17 +0000

    initial commit

---
+++ src/components/navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <div>
+      <h1 style={{color: 'red', fontSize: '22px'}}>
+        hvjasd
+      </h1>
+    </div>
+  )
+}
+
+export default NavBar;
